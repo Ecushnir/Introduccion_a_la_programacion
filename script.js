d35624c2011f0006e344947aa7bf10b5c3eed81d
Ecushnir <49291402+Ecushnir@users.noreply.github.com>
2025-09-22 23:22:29 +0000

    Second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("Submit")
+const input = document.getElementById("inputTexto1")
+console.log(Submit)
+button.onclick = (e) =>{
+    e.preventDefault()
+    console.log (input.value)
+}
